1a6dcff42bb7c3a025614b360126d6f51ea09392
Tasfia Islam <tasf1a027@gmail.com>
2020-11-18 19:10:11 +0600

    newsapi updated

--- news_scraper/news/views.py
+++ news_scraper/news/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
 from newsapi import NewsApiClient
 from bs4 import BeautifulSoup
 import requests
@@ -8,18 +7,19 @@ import re
 
 from decouple import config
 
-#Views
+# Views
+
 
 def home(request):
-      
-    url = "https://www.dhakatribune.com/articles/sport/cricket/" #url
-    source = requests.get(url).text # url source
-        
-    #beautifulsoup
+
+    url = "https://www.dhakatribune.com/articles/sport/cricket/"  # url
+    source = requests.get(url).text  # url source
+
+    # beautifulsoup
     soup = BeautifulSoup(source, 'html.parser')
 
     news_divs = soup.find_all('div', class_='top-news-cont list-para')
-    img_divs =  soup.find_all('div', class_='top-news')
+    img_divs = soup.find_all('div', class_='top-news')
 
     # empty lists to append later
     paras = []
@@ -28,21 +28,20 @@ def home(request):
     detail_urls = []
 
     for i in range(len(news_divs)):
-        
+
         news = news_divs[i]
 
         headings.append(news.a.h4.text)
         paras.append(news.p.text)
         detail_urls.append('https://www.dhakatribune.com'+news.a['href'])
-        
+
         for j in range(len(img_divs)):
             img_section = img_divs[j]
-     
+
             img = img_section.a.img
             img_url = img['src']
             images.append(img_url)
-            
-    
+
     mylist = zip(headings, paras, images, detail_urls)
 
     api = NewsApiClient(api_key=config('API_KEY'))
@@ -63,11 +62,8 @@ def home(request):
         img.append(myarticles['urlToImage'])
         url.append(myarticles['url'])
 
-
     mylist2 = zip(news, desc, img, url)
 
-    context={'mylist':mylist,'mylist2':mylist2}
+    context = {'mylist': mylist, 'mylist2': mylist2}
 
     return render(request, 'news/index.html', context)
-
-
--- news_scraper/news_scraper/settings.py
+++ news_scraper/news_scraper/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'news_scraper.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(BASE_DIR / 'db.sqlite3'),
     }
 }
 
